2bcc5af3876636c15888eb8fe38debd5f70255c3
Flavio Lopes da Silva <digitalblu@gmail.com>
2022-12-23 07:30:13 -0400

    Finalizando Projeto (eu acho...)

--- js/script.js
+++ js/script.js
@@ -1,40 +1,85 @@
-var textInput = document.querySelector("#input-texto");
-var outInput = document.querySelector("#saida-texto");
+const textarea = document.getElementById('texto-encriptado');
+const btnEncrypt = document.getElementById('botao-criptografar');
+const btnDecrypt = document.getElementById('botao-decriptografar');
+const boxCopy = document.getElementById('box-copiar');
+const copyText = document.getElementById('caixa-copiar');
+const btnCopy = document.getElementById('botao-copiar');
+const boxContent = document.getElementById('box-conteudo');
 
-function criptografar() {
-    
-  var texto = textInput.value;
-  
-    //Divide texto em array de strings com 18 caracteres cada
-    var resultadoCriptografia = texto.match(/.{1,18}/g);
-  
-    //Junta as strings com o caractere de quebra de linha
-    resultadoCriptografia = resultadoCriptografia.join("\n");
-  
-    //Substitui as letras 'a', 'b', 'c' e 'e' pelos respectivos caracteres
-    resultadoCriptografia = resultadoCriptografia.replace(/a/g, "@").replace(/b/g , "&").replace(/c/g , "¢").replace(/e/g , "#");
-  
-    //Atualiza o conteúdo da textarea com o resultado
-    document.getElementById('saida-texto').innerHTML = '<textarea readonly id="input-texto">' + resultadoCriptografia + 
-    '</textarea>' + '<button class="botaoCriptografar" id="copiar" onclick="copiar()">Copiar</button>';
-  }
-  
-  function descriptografar() {
-    
-    // O processo para a função descriptografar é o mesmo, basta substituir os caracteres pelas letras originais
-    var texto = textInput.value;
-    var resultadoDescriptografia = texto.match(/.{1,18}/g);
-    resultadoDescriptografia = resultadoDescriptografia.join("\n");
-    resultadoDescriptografia = resultadoDescriptografia.replace(/@/g, "a").replace(/&/g , "b").replace(/¢/g , "c").replace(/#/g , "e");
-  
-    document.getElementById('saida-texto').innerHTML = '<textarea readonly id="input-texto">' + resultadoDescriptografia + 
-    '</textarea>' + '<button class="botaoDescriptografar" id="copiar" onclick="copiar()">Copiar</button>';
-  }
-  
-function copiar(){
-    var copiarTexto = document.getElementById("input-texto");
+let hasText = 0;
 
-    copiarTexto.select();
-    document.execCommand('Copy');
-    alert("Texto Copiado");
+btnEncrypt.addEventListener('click', () => {
+    let text = textarea.value.toLowerCase().normalize('NFD');
+    if(text !== '' && textarea.value === text){
+            let encryptedText = ''
+            text.split('').forEach(e => {
+                switch(e){
+                    case 'a':
+                        encryptedText += e.replace('a', 'ai');
+                        break;
+                    case 'e':
+                        encryptedText += e.replace('e', 'enter');
+                        break;
+                    case 'i':
+                        encryptedText += e.replace('i', 'imes');
+                        break;
+                    case 'o':
+                        encryptedText += e.replace('o', 'ober');
+                        break;
+                    case 'u':
+                        encryptedText += e.replace('u', 'ufat');
+                        break;
+                    default:
+                        encryptedText += e;
+                }
+            });
+        
+            hasText = 1;
+            textVerif(encryptedText);
+    }else{
+        alert('Inserir uma palavra valida para ser criptografada (somente letras minúsculas e sem acento).');
+    }   
+});
+
+btnDecrypt.addEventListener('click', () => {
+    let text = textarea.value.toLowerCase().normalize('NFD');
+    const letters = [['a', 'ai'], ['e', 'enter'], ['i', 'imes'], ['o', 'ober'], ['u', 'ufat']];
+    if(text !== '' && textarea.value === text){        
+        for(let i = 0; i < letters.length; i++) {
+            if(text.includes(letters[i][1])){
+                text = text.replaceAll(letters[i][1], letters[i][0]);
+            }
+        }
+    
+        hasText = 1;
+        textVerif(text);
+    }else{
+        alert('Inserir uma palavra valida para ser descriptografada (somente letras minúsculas e sem acento).');
+    } 
+});
+
+btnCopy.addEventListener('click', () => {
+    const textareaCopy = document.createElement('textarea');
+    const textCopy = copyText.innerText;
+
+    textareaCopy.value = textCopy;
+    document.body.appendChild(textareaCopy);
+    textareaCopy.select();
+    navigator.clipboard.writeText(textareaCopy.value);
+    textareaCopy.remove();
+    alert('Copiado para a area de transferencia!');
+})
+
+function textVerif(eText) {
+    if(hasText > 0) {        
+        boxContent.style.display = 'none';
+        boxCopy.style.display = 'flex';
+        const newText = document.createElement('p');
+        newText.classList.add('copy');
+        newText.textContent = eText;
+        copyText.appendChild(newText);
+    }else{
+        boxContent.style.display = 'block';
+        boxCopy.style.display = 'none';
+    }
 }
